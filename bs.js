37079876bc5fee9f477f4aea59c950e5a4685af6
nusad <jobsicandofromhome@gmail.com>
2017-06-02 23:33:33 +0700

    Update bs.js

--- bs.js
+++ bs.js
@@ -1,2 +1,12 @@
-﻿$.get('bid.css', function (resp) {
-});
+var cssId = 'sales';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'bid.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
