179e16cd68c6bc907a0f45359328a87048a2a5bc
nusad <jobsicandofromhome@gmail.com>
2017-06-07 14:26:32 +0700

    Create sd.js

---
+++ sd.js
@@ -0,0 +1,12 @@
+var cssId = 'sales';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://rawgit.com/nusad/git/master/sd.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
